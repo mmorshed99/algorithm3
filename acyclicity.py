931fcc14954d108897b64577d0103c1afe27f3f3
mmorshed99 <mmorshed@asu.edu>
2017-06-02 21:29:09 -0400

    adding comments

--- acyclicity.py
+++ acyclicity.py
@@ -15,19 +15,16 @@ def acyclic(adj):
        visited.append(y)
     while(len(visited) > 0):
          stack.append(i)
-         #print(stack)
-         #print(i)
-         #print(adj)
+         #### if the node has outgoing connection and that node is already in stack..there is a cycle ####
          if(len(adj[i]) > 0):
-           #for j in range(0,len(adj[i]):
              if adj[i][len(adj[i])-1] in stack:
-                #print(stack)
-                #print(adj[i][len(adj[i])-1])
                 loop = 1
                 visited = []
                 break
+             #### if that node isn't in stack, travelling will be continued ### 
              else:
                 i = adj[i][len(adj[i])-1]
+         ### if that node has no outbound connection...no cycle is detected yet and that node can be excluded from future explorations ###
          else:
              temp = stack.pop()
              total = total - 1
@@ -36,12 +33,12 @@ def acyclic(adj):
                    if adj[q][r] == temp:
                      adj[q].remove(temp)
                      break
-             #print(temp)
-             #print(adj)
+             ### In case of no cycle...this will make sure that loop stops iterating..as there will be nothing in visited in the end ###
              visited.remove(temp)
-             #del visited[temp]
+             ### To resume explorations...a node from stack is selected  ###
              if len(stack)>0:
                i = stack.pop()
+             ### or if stack is empty, any unvisited node is selected, if any ###
              elif len(visited)>0:
                i = visited[len(visited) - 1]        
 
@@ -56,5 +53,4 @@ if __name__ == '__main__':
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
-    #print(adj)
     print(acyclic(adj))
