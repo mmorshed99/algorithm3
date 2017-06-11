e904d0b01c4c161caf595e7aff323199a642e77e
mmorshed99 <mmorshed@asu.edu>
2017-06-11 18:13:02 -0400

    adding comments

--- bfs.py
+++ bfs.py
@@ -14,25 +14,23 @@ def distance(adj, s, t):
       return -1
 
     for i in range(0,len(adj[s])):
-       if adj[s][i] == t:
+       if adj[s][i] == t: # if it is adjacent node, distance is 1
          return 1
-       next_visit.append(adj[s][i])
-       track[adj[s][i]] = 0
+       next_visit.append(adj[s][i]) # next visit will be all neighbouring nodes as implementing bfs
+       track[adj[s][i]] = 0         # tracks if node is already visited
     curr_dist = 1
-    while (len(next_visit)>0):
-         curr_node = next_visit.pop()
+    while (len(next_visit)>0):      # if there is still something to visit
+         curr_node = next_visit.pop() # will select a node from the next_visit list, all of them ar from same level
          for i in range(0,len(adj[curr_node])):
-            if track[adj[curr_node][i]]:
+            if track[adj[curr_node][i]]: # if any child of the neighbouring node is already not visited, it will be visited in future
               future_visit.append(adj[curr_node][i])
-              if adj[curr_node][i] == t:
+              if adj[curr_node][i] == t: # if current node is already the distination, it is done 
                 return curr_dist + 1
               else:
-                track[adj[curr_node][i]] = 0
-         if len(next_visit) == 0:
+                track[adj[curr_node][i]] = 0 # if it isn't the distination, it is marked as visited
+         if len(next_visit) == 0:  # if all nodes from next visit are done, children from visited node will be tried, who are in future_visit
            curr_dist += 1
            next_visit = future_visit
-           #for j in range(0,len(future_visit)):
-              #track[j] =0
            future_visit = []
           
    
